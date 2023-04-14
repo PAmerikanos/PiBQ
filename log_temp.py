9e5af1d2f6f4449affce4ba86887e0b451d6d39c
Paris Amerikanos <pamerikanos@outlook.com>
2023-04-14 21:27:29 +0300

    newline

--- log_temp.py
+++ log_temp.py
@@ -11,6 +11,6 @@ with open('temperature.log', 'w', encoding = 'utf-8') as f:
     while True:
         smoker_temp = m.get_hot_junction_temperature()
         date_time = datetime.now().time()
-        f.write(f"{date_time},{smoker_temp}")
+        f.write(f"{date_time},{smoker_temp}\n")
         f.flush()
         time.sleep(1)
