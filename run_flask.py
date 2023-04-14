8b189308232221b778ad8b99aa78c583bdca48bd
Paris Amerikanos <pamerikanos@outlook.com>
2023-04-14 21:07:36 +0300

    fix ms

--- run_flask.py
+++ run_flask.py
@@ -15,7 +15,7 @@ def hello():
     with open('temperature.csv', newline='') as csvfile:
         data = list(csv.reader(csvfile))
         for row in data:
-            x.append(datetime.strptime(row[0],"%H:%M:%S"))
+            x.append(datetime.strptime(row[0],"%H:%M:%S.%f"))
             y.append(float(row[1]))
 
     # Generate the figure **without using pyplot**.
