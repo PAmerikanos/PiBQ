9403ccb3816307c14544c55d4abd0a718955d51c
Paris Amerikanos <pamerikanos@outlook.com>
2023-04-14 18:58:34 +0300

    started dev on matplotlib

---
+++ log_temp.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import mcp9600
+from datetime import datetime
+from time import time
+import csv
+
+m = mcp9600.MCP9600()
+
+with open('temperature.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(['datetime', 'temperature'])
+
+    while True:
+        smoker_temp = m.get_hot_junction_temperature()
+        time = datetime.now().time()
+        writer.writerow([time, smoker_temp])
+        time.sleep(1)
