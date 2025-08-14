7bbc4147443446154b93bee0c610448eb2fcaea3
Paris Amerikanos <pamerikanos@outlook.com>
2025-08-14 16:23:21 +0300

    fix: time format

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ app.layout = [
 def update_graph(n_clicks, smoker_target_temp, meat_min_temp, past_minutes, forecast_minutes, rolling_avg_window):
     # Parse temperature data
     df = parse_temperature_data()
-    df['datetime'] = pd.to_datetime(df['datetime'], format='mixed').dt.strftime('%H:%M:%S')
+    df['datetime'] = pd.to_datetime(df['datetime'], format='mixed').dt.strftime('%H:%M:%S.%f')
     df['smoker_temp'] = df['smoker_temp'].rolling(window=rolling_avg_window).mean()
     df['meat_temp'] = df['meat_temp'].rolling(window=rolling_avg_window).mean()
 
